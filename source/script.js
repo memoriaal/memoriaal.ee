927e4275f281c89e1eaacdad251a51e4321f8374
Argo Roots <argo@roots.ee>
2018-07-24 15:34:42 +0300

    Display correct text (by hash)

--- source/script.js
+++ source/script.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+function load () {
     var findClass = document.getElementsByClassName('nav-title')
     var tallest = 0
 
@@ -11,19 +11,34 @@ window.onload = function () {
     for(i = 0; i < findClass.length; i++) {
         findClass[i].style.height = tallest + 'px'
     }
+
+    hashChange()
 }
 
-window.onload = function () {
-    var findClass = document.getElementsByClassName('container-fluid')
-    var tallest = 0
+function hashChange () {
+    var textBlocs = document.getElementsByClassName('text-block')
+    var textLinks = document.getElementsByClassName('text-link')
+    var hash = location.hash.replace('#', '')
 
-    for(i = 0; i < findClass.length; i++) {
-        var ele = findClass[i]
-        var eleHeight = ele.offsetHeight
-        tallest = eleHeight > tallest ? eleHeight : tallest
+    for(i = 0; i < textBlocs.length; i++) {
+        textBlocs[i].classList.add('d-none')
     }
 
-    for(i = 0; i < findClass.length; i++) {
-        findClass[i].style.height = tallest + 'px'
+    for(i = 0; i < textLinks.length; i++) {
+        if (textLinks[i].getAttribute('href') === '#' + hash) {
+            textLinks[i].classList.add('active')
+        } else {
+            textLinks[i].classList.remove('active')
+        }
+    }
+
+    if (hash) {
+        var findId = document.getElementById(hash)
+        findId.classList.remove('d-none')
+    } else {
+        textBlocs[0].classList.remove('d-none')
     }
 }
+
+window.onhashchange = hashChange
+window.onload = load
