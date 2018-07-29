0e593fcda5e2fa06d50d0e259cad417310dabf64
Argo Roots <argo@roots.ee>
2018-07-29 17:02:56 +0300

    Use jQuery

--- source/_scripts/script.js
+++ source/_scripts/script.js
@@ -1,46 +1,23 @@
-function load () {
-    document.getElementById('text').style['padding-bottom'] = document.getElementById('navigation').offsetHeight + 'px'
+$(function () {
+    $('#text').css('padding-bottom', $('#navigation').outerHeight(true) + 'px')
 
-    var findClass = document.getElementsByClassName('nav-title')
     var tallest = 0
-
-    for(i = 0; i < findClass.length; i++) {
-        var ele = findClass[i]
-        var eleHeight = ele.offsetHeight
+    $('.nav-title').each(function () {
+        var eleHeight = $(this).outerHeight(true)
         tallest = eleHeight > tallest ? eleHeight : tallest
-    }
+    })
+    $('.nav-title').css('height', tallest + 'px')
 
-    for(i = 0; i < findClass.length; i++) {
-        findClass[i].style.height = tallest + 'px'
-    }
+    $(window).on('hashchange', function () {
+        if (location.hash) {
+            $('.text-link').removeClass('active')
+            $('.text-link[href="' + location.hash + '"]').addClass('active')
 
-    hashChange()
-}
-
-function hashChange () {
-    var textBlocs = document.getElementsByClassName('text-block')
-    var textLinks = document.getElementsByClassName('text-link')
-    var hash = location.hash.replace('#', '')
-
-    for(i = 0; i < textBlocs.length; i++) {
-        textBlocs[i].classList.add('d-none')
-    }
-
-    for(i = 0; i < textLinks.length; i++) {
-        if (textLinks[i].getAttribute('href') === '#' + hash) {
-            textLinks[i].classList.add('active')
+            $('.text-block').addClass('d-none')
+            $(location.hash).removeClass('d-none')
         } else {
-            textLinks[i].classList.remove('active')
+            $('.text-block').first().removeClass('d-none')
         }
-    }
-
-    if (hash) {
-        var findId = document.getElementById(hash)
-        findId.classList.remove('d-none')
-    } else {
-        textBlocs[0].classList.remove('d-none')
-    }
-}
-
-window.onhashchange = hashChange
-window.onload = load
+    })
+    $(window).trigger('hashchange')
+})
