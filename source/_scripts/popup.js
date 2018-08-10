38c3ca9125d009294b8984dc1c5b95fc6121a174
Argo Roots <argo@roots.ee>
2018-08-10 19:36:11 +0300

    Hide opening popup on kiosk url

--- source/_scripts/popup.js
+++ source/_scripts/popup.js
@@ -2,4 +2,8 @@ $(function () {
     $('#popup-close, #popup-background').click(function () {
         $('#popup-background, #popup-content').hide()
     })
+
+    if (window.location.host === 'www.memoriaal.ee') {
+        $('#popup-background, #popup-content').removeClass('d-none')
+    }
 })
