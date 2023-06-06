87169b4d399682dbadeff239346b3fb8e8f78aa8
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-06-06 10:24:39 +0300

    figure out the paths

--- scripts/compile-gallery.js
+++ scripts/compile-gallery.js
@@ -4,26 +4,45 @@ const fs = require('fs')
 const path = require('path')
 const yaml = require('js-yaml')
 
-const galleryPath = path.join(__dirname, '..', 'public', 'images', 'galerii')
+const publicGalleryPath = path.join(__dirname, '..', 'public', 'images', 'galerii')
+const publicGalleryFromPath = path.join(publicGalleryPath, 'source')
+const publicGalleryToPath = path.join(publicGalleryPath, 'public')
 const gallerySourcePath = path.join(__dirname, '..', 'src', 'galerii')
-const galleryYamlPath = path.join(__dirname, '..', 'src', 'galerii', 'galerii.yaml')
+const galleryYamlPath = path.join(gallerySourcePath, 'galerii.yaml')
 
-const galleryFiles = fs.readdirSync(galleryPath)
+const galleryFiles = fs.readdirSync(publicGalleryFromPath)
+
+// remove extension from filename, then slugify, then add extension back
+const slugify = (str) => {
+    const ext = path.extname(str)
+    const name = path.basename(str, ext)
+    return `${name.toLowerCase().replace(/[^a-z0-9]+/g, '-')}${ext}`
+}
+// const slugify = (str) => {
+//     return str
+//     .toLowerCase()
+//     .replace(/ /g, '_')
+//     .replace(/[^\w-]+/g, '')
+// }
 
 const galleryData = galleryFiles
 .filter((file) => {
-    // remove files that are not images
+    // skip files that are not images
     return file.match(/\.(jpg|jpeg|png|gif)$/i)
 })
 .map((file) => {
-    const filePath = path.join(galleryPath, file)
-    const fileStat = fs.statSync(filePath)
+    const newFileName = slugify(file)
+    // copy files to src/galerii
+    const fileFromPath = path.join(publicGalleryFromPath, file)
+    const fileToPath = path.join(publicGalleryToPath, newFileName)
+    fs.copyFileSync(fileFromPath, fileToPath)
+    const fileStat = fs.statSync(fileFromPath)
     const fileDate = new Date(fileStat.mtime)
     // filenames should be formatted like: "The title of the picture - Author.jpg"
-    const [title, author] = file.split('.').slice(0, -1).join('.').split(' - ')
+    const [title, author] = file.split('.').slice(0, -1).join('.').split(' - ').map((str) => str.trim())
 
     return {
-        file: file,
+        file: newFileName,
         title: title,
         author: author,
         date: fileDate,
