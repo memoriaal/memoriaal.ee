83b57eb379f9f93d50be8b9d2add06eaa4d13ac7
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-11-07 14:54:09 +0200

    search by episoodid

---
+++ src/_scripts/helpers.js
@@ -0,0 +1,23 @@
+const copy2clipboard = function (DomE, transparentE = null) {
+    if (!DomE) {
+        console.error('copy2clipboard: missing DomE')
+        return
+    }
+    const clip = DomE.getAttribute('clip')
+    if (!clip) {
+        console.error('copy2clipboard: missing clip')
+        return
+    }
+    navigator.clipboard.writeText(clip)
+    transparentE = transparentE || DomE.firstChild?.nextSibling
+    if (transparentE) {
+        transparentE.classList.remove('transparent')
+        setTimeout(() => { transparentE.classList.add('transparent') }, 400)
+    }
+}
+
+const qs = function (key) {
+    key = key.replace(/[*+?^$.\[\]{}()|\\\/]/g, "\\$&")
+    var match = location.search.match(new RegExp("[?&]"+key+"=([^&]+)(&|$)"))
+    return match && decodeURIComponent(match[1].replace(/\+/g, " "))
+}
