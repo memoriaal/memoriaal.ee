f763f4fd5298a72dee622f3835ec41caa7b8b9da
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-11-10 15:58:58 +0200

    styles

--- src/_scripts/select.js
+++ src/_scripts/select.js
@@ -1,5 +1,10 @@
 $(function () {
-    hosts = ['www.memoriaal.ee', 'memoriaal.ee', 'localhost:4000']
+    hosts = [
+        'www.memoriaal.ee',
+        'memoriaal.ee',
+        'localhost:4000',
+        'https://feature-episoodid--memoriaal.netlify.app/'
+    ]
     if (hosts.indexOf(window.location.host) === -1) {
         $('body').css({
             '-moz-user-select': 'none',
